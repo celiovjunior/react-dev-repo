04a138051df6c9c933c82d2caab3e43eaa97e5f7
Celio Junior <celio_cjr@hotmail.com>
2022-06-06 22:02:37 -0300

    installing jsonwebtoken and building the authConfig service

--- server/src/middlewares/auth.js
+++ server/src/middlewares/auth.js
@@ -1,13 +1,18 @@
 /**
- * Fluxo do middleware de autenticacao:
+ * Fluxo do middleware de autenticação:
  * Client <--> Node.js -> Express -> Middlewares -> Controller
  */
+import jwt from 'jsonwebtoken';
+import authConfig from '../config/auth';
+
 export default async (req, res, next) => {
   const authenticated = true;
 
   if(authenticated) {
     return next();
-    // next eh a funcao q vai liberar o fluxo caso a req de certo
+    // 'next' é a função que vai liberar o fluxo, caso a condição seja 'true'
+  } else {
+    return res.status(401).json();
   }
 
 }
