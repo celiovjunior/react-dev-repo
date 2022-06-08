e9a5117c0c798e7a7a388013ebc478b2747fec28
Celio Junior <celio_cjr@hotmail.com>
2022-06-07 22:01:45 -0300

    first test of the handleLogin works fine

--- client/src/pages/LoginPage/index.jsx
+++ client/src/pages/LoginPage/index.jsx
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import './styles.css';
 
 function LoginPage() {
-  const handleLogin = (e) => {
-    e.preventDefault();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = (event) => {
+    event.preventDefault();
+    console.log('Email: ', email)
+    console.log('Password: ', password)
     console.log('evento disparado')
   }
 
@@ -11,14 +16,26 @@ function LoginPage() {
     <div id="login">
       <h1 className="title">Login</h1>
       <form action="" className="form">
-        <fieldset className="field">
+        <div className="field">
           <label htmlFor="email">E-mail:</label>
-          <input type="email" name="email" id="email" />
-        </fieldset>
-        <fieldset className="field">
+          <input
+           type="email" 
+           name="email" 
+           id="email" 
+           value={email}
+           onChange={(e) => setEmail(e.target.value)}
+           />
+        </div>
+        <div className="field">
           <label htmlFor="password">Senha:</label>
-          <input type="password" name="password" id="password" />
-        </fieldset>
+          <input
+           type="password" 
+           name="password" 
+           id="password" 
+           value={password} 
+           onChange={(e) => setPassword(e.target.value)} 
+          />
+        </div>
         <div className="actions">
           <button onClick={handleLogin}>Entrar</button>
         </div>
