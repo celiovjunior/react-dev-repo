ca4479676318f5dafaa18bcfdf45562fd55441c4
Celio Junior <celio_cjr@hotmail.com>
2022-06-10 12:02:59 -0300

    turn off the auth middleware to made tests, connecting client side with server side

--- client/src/pages/MainPage/index.jsx
+++ client/src/pages/MainPage/index.jsx
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Nav from "../../components/Nav";
 import Repositories from "../../components/Repositories";
 import Search from "../../components/Search";
+import { getRepositories } from "../../services/api";
 import './styles.css';
 
+const userId = '628d32f560b0c4462b9749e6'
+
 function MainPage() {
+  const [repositories, setRepositories] = useState([]);
+
+  const loadData = async (query = '') => {
+    const response = await getRepositories(userId);
+
+    setRepositories(response.data);
+  }
+
+  useEffect(() => {
+    // função anônima para execução do loadData, já que o useEffect não executa async-await de fora
+    (async () => await loadData())();
+  }, []);
+
   const handleLogout = () => {
     console.log('saiu do sistema')
   }
